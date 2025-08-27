1decbf7575783befcedf6d186576c08181b49971
AvniUplabdhee <avnigungun@gmail.com>
2025-08-27 17:49:56 +0530

    first app

---
+++ newapp/App.tsx
@@ -0,0 +1,23 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+import React from 'react'
+import {View,
+        Text,
+} from 'react-native'
+import { SafeAreaView } from "react-native-safe-area-context";
+
+function App(){
+  return(
+  <SafeAreaView>
+    <View>
+      <Text>Hello World</Text>
+    </View>
+  </SafeAreaView>
+  )
+}
+
+export default App
